1285e43bd114e32f1ad544af556e67dac88d608c
ilciro <danielecinus10@gmail.com>
2022-10-28 12:01:57 +0200

    inserisco main

--- LibreriaWeb/src/main/java/boundary/Main.java
+++ LibreriaWeb/src/main/java/boundary/Main.java
@@ -1,11 +1,53 @@
 package boundary;
 
-public class Main {
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Cciao");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import utilities.CreateDefaultDB;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage primaryStage) {
+		Scene scene;
+
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
+			scene = new Scene(root);
+			primaryStage.setTitle("Benvenuto nella homePage");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			
+		}
 
 	}
 
+	public static void main(String[] args)  {
+		//uso status per vedere se trigger creati
+		 
+		
+
+		
+		try {
+			CreateDefaultDB.createDefaultDB();			
+
+		} catch (FileNotFoundException |SQLException  eFile) {
+			eFile.printStackTrace();
+		}
+		
+
+		launch(args);
+		
+		
+		
+	}
 }
